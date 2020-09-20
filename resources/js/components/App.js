53a849f42a6ac36910b08069e9d01eee4dbc07ec
bpgity <bhagwanpawar07@gmail.com>
2020-09-20 12:40:59 +0530

    first commit

---
+++ resources/js/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+}
+
+export default App;
+ReactDOM.render(<App />, document.getElementById("app"));
